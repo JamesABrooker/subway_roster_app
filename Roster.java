ba5ee710ae5a2e01c7c19300a90df20eecc57cf7
James Brooker <jamesabrooker@gmail.com>
2025-07-02 15:25:05 +1000

    Started defining constructor

--- Roster.java
+++ Roster.java
@@ -1,8 +1,27 @@
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-record Roster(Set<Day> days, Set<Worker> workers) {
-    public Roster{
+class Roster {
+    private final Set<Day> days;
+    private Set<Worker> workers;
+    private Map<Day,List<Hours>> coveredHours = new HashMap<>();
 
+    public Roster(Set<Day> days, Set<Worker> workers){
+        this.days = days;
+        this.workers = workers;
+        for(Day day : days) {
+            List<Hours> hoursInDay = new ArrayList<>();
+            hoursInDay.add(day.hours);
+            coveredHours.put(day, hoursInDay);
+        }
+
+        for(Worker worker : workers) {
+            for(Shift s: worker.shifts()) addHours(s);
+        }
     }
+
+    public void addHours(Shift shift){
+        coveredHours.put(shift.day(),shift.hours().replaceHour(coveredHours.get(shift.day())));
+    }
+
+
 }
